3775063a51579bc39b67ff9ada72d703bb4f6841
Dris Ait Oujkal <drisaitoujkal@Driss-MacBook-Pro.local>
2023-09-18 12:08:53 +0100

    project versioning

---
+++ src/app/receipes/receipe-detail/receipe-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Receipe } from '../receipe.model';
+
+@Component({
+  selector: 'app-receipe-detail',
+  templateUrl: './receipe-detail.component.html',
+  styleUrls: ['./receipe-detail.component.css']
+})
+export class ReceipeDetailComponent implements OnInit {
+
+  @Input() receipe : Receipe;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
